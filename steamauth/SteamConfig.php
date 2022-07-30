23529e6b5eeb244ffc0bf706fd71a23745d9e2a3
Koo Minu <44253739+umin135@users.noreply.github.com>
2022-07-30 11:50:14 +0900

    Update SteamConfig.php

--- steamauth/SteamConfig.php
+++ steamauth/SteamConfig.php
@@ -1,9 +1,9 @@
 <?php
 //Version 3.2
-$steamauth['apikey'] = ""; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
-$steamauth['domainname'] = ""; // The main URL of your website displayed in the login page
-$steamauth['logoutpage'] = ""; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
-$steamauth['loginpage'] = ""; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['apikey'] = "49BD64645E38254BA6A5038173E3DC4D"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
+$steamauth['domainname'] = "https://umin135.github.io/Nier_Fan_Information_KR/"; // The main URL of your website displayed in the login page
+$steamauth['logoutpage'] = "index.html"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['loginpage'] = "index.html"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
 // System stuff
 if (empty($steamauth['apikey'])) {die("<div style='display: block; width: 100%; background-color: red; text-align: center;'>SteamAuth:<br>Please supply an API-Key!<br>Find this in steamauth/SteamConfig.php, Find the '<b>\$steamauth['apikey']</b>' Array. </div>");}
